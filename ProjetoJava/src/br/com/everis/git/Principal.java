6758a76c78fa4ba14a81718ac1ca5d8828d4728c
tvieiras <tiago.vieira.sousa@everis.com>
2017-10-17 12:03:26 -0200

    [commit]

--- ProjetoJava/src/br/com/everis/git/Principal.java
+++ ProjetoJava/src/br/com/everis/git/Principal.java
@@ -1,11 +1,15 @@
 package br.com.everis.git;
 
+import br.com.modulo.teste.Modulo;
+
 public class Principal {
 
 	public static void main(String[] args) {
 
 		System.out.println("#Resolver conflitos");
-		System.out.println("#Msg meio");
+		System.out.println("#Msg meio");		
+		System.out.println("#Inicio!");
+		System.out.println(new Modulo().msg());
 		System.out.println("#fim!");
 
 	}
