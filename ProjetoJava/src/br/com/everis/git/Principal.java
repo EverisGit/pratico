2ffa338baa65f560f57e50b0b5ac26952b318b31
aribeira <alexsander.ribeiro.avila@everis.com>
2017-10-17 11:30:47 -0200

    [Primeiro]

---
+++ ProjetoJava/src/br/com/everis/git/Principal.java
@@ -0,0 +1,13 @@
+package br.com.everis.git;
+
+public class Principal {
+	
+	public static void main(String []args){
+		
+		System.out.println("#Inicio!");
+		
+		System.out.println("#fim!");
+		
+	}
+
+}
